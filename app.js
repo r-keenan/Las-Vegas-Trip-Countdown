035637da971cb032fa27b3065ed4c299365e6f4b
Ross Keenan <ross.keenan@protonmail.com>
2021-05-29 14:41:34 -0500

    Finished business logic and setInterval for timer.

---
+++ app.js
@@ -0,0 +1,29 @@
+const countdown = () => {
+  //Setting date variables
+  const countDate = new Date("June 17, 2021 10:45:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  //Setting time variables
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  //Business logic for time calculations
+  const textDay = Math.floor(gap / day);
+  const textHour = Math.floor((gap % day) / hour);
+  const textMinute = Math.floor((gap % hour) / minute);
+  const textSecond = Math.floor((gap % minute) / second);
+
+  //Filling the DOM with the data.
+  document.querySelector(".day").innerText = textDay;
+  document.querySelector(".hour").innerText = textHour;
+  document.querySelector(".minute").innerText = textMinute;
+  document.querySelector(".second").innerText = textSecond;
+
+  //   console.log(gap);
+};
+
+//This sets the timer to update every second.
+setInterval(countdown, 1000);
